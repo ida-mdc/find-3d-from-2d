c783ada3a2bd2c607bb511bc1d99a6e13bf3c140
Kyle Harrington <mdc@kyleharrington.com>
2021-11-26 08:18:51 +0100

    Output an arguments json file

--- generate_album_arguments.py
+++ generate_album_arguments.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yaml
 import os
+import json
 
 # From functions_common module
 
@@ -39,17 +40,22 @@ print(key_file.head())
 
 data_path = parameters["data_folder"]
 folder_2d_data = "/03_Preprocessed_Data/01_2D/"
+folder_3d_data = "/03_Preprocessed_Data/02_3D/"
+folder_macrophage_annotations = '/04_Processed_Data/01_Annotated_Macrophages/'
 use_gpu = parameters["use_gpu"]
 output_folder = parameters["output_folder"]
 experiments = "annotated"
 #experiments = "all"
 
+param_list = []
+
 for index, row in key_file.iterrows():
 
     short_name = str(row["short_name"])
-    file_path = data_path + folder_2d_data + short_name + ".tif"
-
-    print([index, row])
+    file_path = data_path + folder_3d_data + short_name + ".tif"
+    csv_path = data_path + folder_macrophage_annotations + short_name + ".tif"
+    
+    # print([index, row])
 
     if not os.path.exists(file_path):
         continue
@@ -58,4 +64,10 @@ for index, row in key_file.iterrows():
         print("No annotation available for %s" % short_name)
         continue
 
-    print("open image file %s" % file_path)
+    # print("open image file %s" % file_path)
+
+    args = {'image_filename':file_path, 'csv_filename':csv_path}
+    param_list += [args]
+
+with open('arguments.json', 'w') as fout:
+    json.dump(param_list, fout)
